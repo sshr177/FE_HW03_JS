23589ee802c84609d9e7bfdceed51358fb1a06c9
dogdevelophoyri <hyori7272@gmail.com>
2026-05-12 21:12:31 +0900

    수정

--- session_03_HW.js
+++ session_03_HW.js
@@ -1,19 +1,19 @@
 const person = {
     name : '아기사자',
-    age : 20,
+    age : 13,
 
     introduce: function () {
-        console.log('제 이름은 ${this.name}이고, 나이는 ${this.age}살입니다.');
+        console.log(`제 이름은 ${this.name}이고, 나이는 ${this.age}살입니다.`);
     }
 };
 
 const horse = {
-    name = '적토마',
-    age = 3,
-    rider = person,
+    name : '적토마',
+    age : 3,
+    rider : person,
 
     introduce: function (){
-        console.log('이 말의 이름은 ${this.name}이고, 나이는 ${this.age}살입니다.');
+        console.log(`이 말의 이름은 ${this.name}이고, 나이는 ${this.age}살입니다.`);
     }
 };
 
